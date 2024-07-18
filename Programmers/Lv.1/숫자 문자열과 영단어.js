d31015446319a4dc024afc9507c74520f2a5c04b
Xoeon <jayy_19@ewhain.net>
2024-07-18 21:54:59 +0900

    [Refactor]Programmers Lv.1: 숫자 문자열과 영단어

--- Programmers/Lv.1/숫자 문자열과 영단어.js
+++ Programmers/Lv.1/숫자 문자열과 영단어.js
@@ -1,17 +1,17 @@
-const num = {
-  0: "zero",
-  1: "one",
-  2: "two",
-  3: "three",
-  4: "four",
-  5: "five",
-  6: "six",
-  7: "seven",
-  8: "eight",
-  9: "nine",
-};
+const num = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
 
-const solution = (s) => {
+const solution1 = (s) => {
   const num_l = Object.keys(num).length;
   const l = s.length;
 
@@ -33,4 +33,37 @@ const solution = (s) => {
   }
 
   return +answer.join("");
+};
+
+const solution2 = (s) => {
+  const l = num.length;
+  let answer = s;
+
+  for (let i = 0; i < l; i++) {
+    let arr = answer.split(num[i]);
+    answer = arr.join(i);
+  }
+
+  return +answer;
+};
+
+const solution3 = (s) => {
+  let charSet = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  for (let [key, value] of Object.entries(charSet)) {
+    let re = new RegExp(key, "g");
+    s = s.replace(re, value);
+  }
+  return +s;
 };
