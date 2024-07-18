36a0cedb8f60ddafba644af0511c71c019792169
Xoeon <jayy_19@ewhain.net>
2024-07-18 21:55:13 +0900

    [Refactor]Programmers Lv.1: 가장 가까운 글자

--- Programmers/Lv.1/가장 가까운 글자.js
+++ Programmers/Lv.1/가장 가까운 글자.js
@@ -1,19 +1,10 @@
 const solution = (s) => {
-  const l = s.length;
-  let answer = [-1]; // 첫 글자는 무조건 -1
+  const hash = {};
 
-  for (let i = 1; i < l; i++) {
-    for (let j = i - 1; j >= 0; j--) {
-      if (s[i] === s[j]) {
-        answer.push(i - j);
-        break;
-      }
-    }
-    if (answer.length !== i + 1) {
-      // 중첩 반복문을 다 돌 때까지 push된 요소가 없으면 -1
-      answer.push(-1);
-    }
-  }
+  return [...s].map((v, i) => {
+    let result = hash[v] !== undefined ? i - hash[v] : -1;
+    hash[v] = i;
 
-  return answer;
+    return result;
+  });
 };
