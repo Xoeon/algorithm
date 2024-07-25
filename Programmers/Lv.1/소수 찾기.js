28ea6ab9ad5f0b447636eca7c48969872582824a
Xoeon <jayy_19@ewhain.net>
2024-07-25 16:41:05 +0900

    Programmers Lv.1: 소수 찾기

---
+++ Programmers/Lv.1/소수 찾기.js
@@ -0,0 +1,16 @@
+const solution = (n) => {
+  if (n < 2) return 0;
+
+  let sieve = new Array(n + 1).fill(true);
+  sieve[0] = sieve[1] = false;
+
+  for (let i = 2; i * i <= n; i++) {
+    if (sieve[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        sieve[j] = false;
+      }
+    }
+  }
+
+  return sieve.filter(Boolean).length;
+};
