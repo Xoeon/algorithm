2d621cb9bc94a81709bec8f06552ad5f91188294
소연 <azurespring@soyeon-ui-MacBookPro.local>
2024-08-09 10:37:29 +0900

    Programmers Lv2: 소수 찾기

---
+++ Programmers/Lv.2/소수 찾기.js
@@ -0,0 +1,36 @@
+const isPrime = (n) => {
+  if (n < 2) return false;
+  
+  for (let i = 2; i * i <= n; i++) {
+      if (n % i === 0) {
+          return false;
+      }
+  }
+  
+  return true;
+}
+
+const solution = (numbers) => {
+  const answer = new Set();
+  let nums = numbers.split('');
+  
+  const getPermutation = (arr, fixed) => {
+      if (arr.length >= 1) {
+          for (let i = 0; i < arr.length; i++) {
+              const newFixed = fixed + arr[i];
+              const copyArr = [...arr];
+              copyArr.splice(i, 1);
+              
+              if (!answer.has(parseInt(newFixed)) && isPrime(parseInt(newFixed))) {
+                  answer.add(parseInt(newFixed));
+              }
+              
+              getPermutation(copyArr, newFixed);
+          }
+      }
+  }
+  
+  getPermutation(nums, '')
+  
+  return answer.size;
+}
