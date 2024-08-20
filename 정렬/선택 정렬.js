23e00583573ec7b40b58f124ee2816c971e7a688
soyeon <jayy_19@ewhain.net>
2024-08-20 11:52:57 +0900

    선택 정렬 이론

---
+++ 정렬/선택 정렬.js
@@ -0,0 +1,13 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8];
+
+for (let i = 0; i < array.length; i++) {
+  let min_index = i;
+  for (let j = i + 1; j < array.length; j++) {
+    if (array[j] < array[min_index]) {
+      min_index = j;
+    }
+    [array[i], array[min_index]] = [array[min_index], array[i]];
+  }
+}
+
+console.log(array);
