521c27de423bc4ad5d12956c53ecf296e3b54413
soyeon <jayy_19@ewhain.net>
2024-08-26 17:59:17 +0900

    이진 탐색 이

---
+++ 이진 탐색/이진 탐색.js
@@ -0,0 +1,15 @@
+const binary_serach = (array, target, start, end) => {
+  if (start > end) {
+    return;
+  }
+
+  mid = (start + end) / 2;
+
+  if (array[mid] === target) {
+    return mid;
+  } else if (array[mid] > target) {
+    return binary_search(array, target, start, mid - 1);
+  } else {
+    return binary_search(array, target, mid + 1, end);
+  }
+};
