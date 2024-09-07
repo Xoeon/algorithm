437c184c2190df23f0c455087e2c07db02307577
soyeon <jayy_19@ewhain.net>
2024-09-07 13:27:38 +0900

    [Refactor]동빈책: 1로 만들기

--- 다이나믹 프로그래밍/1로 만들기.js
+++ 다이나믹 프로그래밍/1로 만들기.js
@@ -1,35 +1,25 @@
 let x = 26;
 const hash = [];
 
+hash[1] = 0;
 hash[2] = 1;
 hash[3] = 1;
 hash[5] = 1;
 
-const dp = (x) => {
-  let temp = [];
-
-  if (!hash[x]) {
-    if (x % 5 === 0) {
-      console.log('5로 나눈다.');
-      temp.push(hash[x / 5]);
-    }
-    if (x % 3 === 0) {
-      console.log('3로 나눈다.');
-      temp.push(hash[x / 3]);
-    }
-    if (x % 2 === 0) {
-      console.log('2로 나눈다.');
-      temp.push(hash[x / 2]);
-    }
-
-    temp.push(hash[x - 1]);
-
-    hash[x] = Math.min(...temp) + 1;
-  }
-};
-
 for (let i = 4; i <= x; i++) {
-  dp(i);
+  hash[i] = hash[i - 1];
+
+  if (i % 5 === 0) {
+    hash[i] = Math.min(hash[i], hash[i / 5]);
+  }
+  if (i % 3 === 0) {
+    hash[i] = Math.min(hash[i], hash[i / 3]);
+  }
+  if (i % 2 === 0) {
+    hash[i] = Math.min(hash[i], hash[i / 2]);
+  }
+
+  hash[i]++;
 }
 
 console.log(hash[x]);
