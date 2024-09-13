8c0667c4bf1f8959c70b6b43e2de282b0ad5107b
soyeon <jayy_19@ewhain.net>
2024-09-13 15:55:59 +0900

    [Refactor]동빈책: 전보

--- 최단 경로/전보.js
+++ 최단 경로/전보.js
@@ -1,4 +1,4 @@
-class MinHeap {
+class Heap {
   constructor(comparatorFunction) {
     this.heap = [];
     this.compare = comparatorFunction || ((a, b) => a[1] - b[1]);
@@ -31,18 +31,11 @@ class MinHeap {
 
   heapifyUp() {
     let index = this.heap.length - 1;
+    let parentIndex = Math.floor((index - 1) / 2);
 
     while (index > 0) {
-      const element = this.heap[index];
-      const parentIndex = Math.floor((index - 1) / 2);
-      const parent = this.heap[parentIndex];
-
-      if (this.compare(parent, element) >= 0) {
-        break;
-      }
-
-      this.heap[index] = parent;
-      this.heap[parentIndex] = element;
+      if (this.compare(this.heap[parentIndex], this.heap[index]) <= 0) break; // 부모가 더 작으면 종료
+      this.swap(index, parentIndex);
       index = parentIndex;
     }
   }
@@ -50,36 +43,31 @@ class MinHeap {
   heapifyDown() {
     let index = 0;
     const length = this.size();
-    const element = this.heap[0];
 
     while (true) {
-      const leftChildIndex = index * 2 + 1;
-      const rightChildIndex = index * 2 + 2;
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
 
-      let leftChild, rightChild;
-      let swap = null;
+      let swapIndex = index;
 
-      if (leftChildIndex < length) {
-        leftChild = this.heap[leftChildIndex];
-        if (this.compare(leftChild, element) > 0) {
-          swap = leftChildIndex;
-        }
+      if (
+        leftChildIndex < length &&
+        this.compare(this.heap[leftChildIndex], this.heap[swapIndex]) < 0
+      ) {
+        swapIndex = leftChildIndex;
       }
 
-      if (rightChildIndex < length) {
-        rightChild = this.heap[rightChildIndex];
-        if (
-          (swap === null && this.compare(rightChild, element) < 0) ||
-          (swap !== null && this.compare(rightChild, leftChild) < 0)
-        ) {
-          swap = rightChildIndex;
-        }
+      if (
+        rightChildIndex < length &&
+        this.compare(this.heap[rightChildIndex], this.heap[swapIndex]) < 0
+      ) {
+        swapIndex = rightChildIndex;
       }
 
-      if (swap === null) break;
+      if (swapIndex === index) break;
 
-      this.swap(index, swap);
-      index = swap;
+      this.swap(index, swapIndex);
+      index = swapIndex;
     }
   }
 }
@@ -88,16 +76,14 @@ class MinHeap {
 
 const INF = 1e9; // 무한을 의미하는 값으로 10억 설정
 
-const input = require('fs').readFileSync('./input2.txt').toString().split('\n');
-const [n, m] = input[0].split(' ').map(Number);
+const input = require('fs').readFileSync('./input.txt').toString().split('\n');
+const [n, m, c] = input[0].split(' ').map(Number);
 const arr = input.slice(1).map((v) => v.split(' ').map(Number));
 
 const graph = Array.from({ length: n + 1 }, () => []);
-
-const visited = Array(n + 1).fill(false);
 const distance = Array(n + 1).fill(INF);
 
-const start = 1;
+const start = c;
 
 for (let i = 0; i < m; i++) {
   const [a, b, c] = arr[i];
@@ -105,15 +91,14 @@ for (let i = 0; i < m; i++) {
 }
 
 function dijkstra(start) {
-  const pq = new MinHeap();
+  const pq = new Heap();
   pq.enqueue([start, 0]);
   distance[start] = 0;
 
   while (pq.size() > 0) {
     const [current, dist] = pq.dequeue();
 
-    if (visited[current]) continue;
-    visited[current] = true;
+    if (distance[current] < dist) continue;
 
     graph[current].forEach(([neighbor, weight]) => {
       const cost = dist + weight;
@@ -128,10 +113,7 @@ function dijkstra(start) {
 
 dijkstra(start);
 
-for (let i = 1; i <= n; i++) {
-  if (distance[i] === INF) {
-    console.log('INFINITY');
-  } else {
-    console.log(distance[i]);
-  }
-}
+console.log(
+  distance.filter((d) => d !== INF).length - 1,
+  Math.max(...distance.filter((d) => d !== INF))
+);
