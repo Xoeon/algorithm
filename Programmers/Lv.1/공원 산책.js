1c67f7291f424fee3c968084d0193cfdd4fe9ce9
soyeon <jayy_19@ewhain.net>
2024-10-29 13:01:58 +0900

    Programmers Lv1: 공원 산책

--- Programmers/Lv.1/공원 산책.js
+++ Programmers/Lv.1/공원 산책.js
@@ -1,19 +1,42 @@
 function solution(park, routes) {
   let [x, y] = [0, 0];
+
+  const h = park.length;
+  const w = park[0].length;
+
   for (let i = 0; i < park.length; i++) {
     for (let j = 0; j < park[i].length; j++) {
       if (park[i][j] === 'S') [x, y] = [i, j];
     }
   }
 
-  function move(direction) {
-    if (direction === 'E') x++;
-    else if (direction === 'W') x--;
-    else if (direction === 'N') y++;
-    else y--;
-  }
-
   routes.map((route) => {
-    const [d, n] = route.split(' ');
+    const [direction, distance] = route.split(' ');
+    let [dx, dy] = [0, 0];
+
+    if (direction === 'E') dy = 1;
+    else if (direction === 'W') dy = -1;
+    else if (direction === 'S') dx = 1;
+    else dx = -1;
+
+    let canMove = true;
+
+    for (let i = 1; i <= distance; i++) {
+      const a = x + dx * i;
+      const b = y + dy * i;
+
+      if (a >= 0 && a < h && b >= 0 && b < w && park[a][b] !== 'X') continue;
+      else {
+        canMove = false;
+        break;
+      }
+    }
+
+    if (canMove) {
+      x += dx * distance;
+      y += dy * distance;
+    }
   });
+
+  return [x, y];
 }
