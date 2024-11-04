b47619a1eb4da003d9dc84b336e09f004e62e180
soyeon <jayy_19@ewhain.net>
2024-11-04 22:02:42 +0900

    [Refactor] Programmers Lv1: 크레인 인형뽑기 게임

--- Programmers/Lv.1/크레인 인형뽑기 게임.js
+++ Programmers/Lv.1/크레인 인형뽑기 게임.js
@@ -1,8 +1,9 @@
 function solution(board, moves) {
-  const arr = [[]];
-  const basket = [];
-  let count = 0;
+  const arr = [[]]; // board를 line별로 정렬할 배열
+  const basket = []; // 뽑은 인형을 쌓을 바구니
+  let count = 0; // 터진 인형 개수 카운트
 
+  // board 배열을 line별로 정렬
   for (let i = 0; i < board.length; i++) {
     let temp = [];
     for (let j = board.length - 1; j >= 0; j--) {
@@ -13,16 +14,19 @@ function solution(board, moves) {
     arr.push(temp);
   }
 
+  // moves 배열을 순회하면서 인형을 바구니에 담는다.
   for (let move of moves) {
-    const pop = arr[move].pop();
+    const pop = arr[move].pop(); // 해당 line에 가서 가장 상단에 쌓인 인형을 pop
     const len = basket.length;
 
-    if (!pop) continue;
+    if (!pop) continue; // 만약 뽑힌 인형이 없으면 다음 순회로 건너뛴다.
 
     if (len > 0 && basket[len - 1] === pop) {
-      basket.pop();
-      count += 2;
+      // 적어도 쌓인 인형이 1개 이상이고, 뽑은 인형과 바구니의 최상단 인형이 같을 경우
+      basket.pop(); // basket에 쌓인 인형을 pop
+      count += 2; // 터진 인형 수 카운트
     } else {
+      // 그게 아니면 바구니에 인형을 담는다.
       basket.push(pop);
     }
   }
