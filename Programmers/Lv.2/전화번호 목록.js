0e80e021f92273f7171f35a1029467965e217e55
soyeon <jayy_19@ewhain.net>
2025-01-13 15:27:42 +0900

    Programmers Lv2: 전화번호 목록

--- Programmers/Lv.2/전화번호 목록.js
+++ Programmers/Lv.2/전화번호 목록.js
@@ -1,17 +1,9 @@
 function solution(phone_book) {
-  const len = phone_book.length;
-  for (let i = 0; i < len; i++) {
-    const i_len = phone_book[i].length;
-    for (let j = 1; i + j < len; j++) {
-      for (let k = 0; k < i_len; k++) {
-        if (phone_book[i][k] !== phone_book[i + j][k]) {
-          break;
-        } else {
-          if (k === phone_book[i].length - 1) {
-            return false;
-          }
-        }
-      }
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
+      return false;
     }
   }
 
