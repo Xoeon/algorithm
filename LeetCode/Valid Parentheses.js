91b8b8b99e37952b0d59b25febbcc21da5d7effb
soyeon <jayy_19@ewhain.net>
2025-02-06 15:25:22 +0900

    LeetCode: 20. Valid Parentheses

--- LeetCode/Valid Parentheses.js
+++ LeetCode/Valid Parentheses.js
@@ -3,37 +3,16 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-  const stack1 = [];
-  const stack2 = [];
-  const stack3 = [];
+  const stack = [];
+  const map = { ')': '(', '}': '{', ']': '[' };
 
-  s.split('').forEach((bracket) => {
-    switch (bracket) {
-      case '(':
-        stack1.push(bracket);
-      case ')':
-        if (stack1.length === 0 || stack1[stack1.length - 1] !== '(') {
-          return false;
-        } else stack1.pop();
-        break;
-      case '{':
-        stack2.push(bracket);
-      case '}':
-        if (stack2.length === 0 || stack2[stack2.length - 1] !== '{') {
-          return false;
-        } else stack2.pop();
-        break;
-      case '[':
-        stack3.push(bracket);
-      case ']':
-        if (stack3.length === 0 || stack3[stack3.length - 1] !== '[') {
-          return false;
-        } else stack3.pop();
-        break;
-      default:
-        console.log('default');
+  for (const bracket of s) {
+    if (bracket in map) {
+      if (stack.pop() !== map[bracket]) return false;
+    } else {
+      stack.push(bracket);
     }
-  });
+  }
 
-  return true;
+  return stack.length === 0;
 };
