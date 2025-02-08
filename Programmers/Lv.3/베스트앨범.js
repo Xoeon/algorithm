1a5441d3e088b7805c94abf77ebc079ad4227dce
soyeon <jayy_19@ewhain.net>
2025-02-08 16:21:35 +0900

    [Fix] Programmers Lv2: 베스트앨범

--- Programmers/Lv.3/베스트앨범.js
+++ Programmers/Lv.3/베스트앨범.js
@@ -29,42 +29,6 @@ function solution(genres, plays) {
       if (hash[genre][i]) {
         ans.push(plays.indexOf(hash[genre][i]));
       }
-      function solution(genres, plays) {
-        const hash = {};
-        const count = {};
-        const n = genres.length;
-
-        for (let i = 0; i < n; i++) {
-          if (!hash[genres[i]]) {
-            hash[genres[i]] = [[plays[i], i]];
-          } else {
-            hash[genres[i]].push([plays[i], i]);
-          }
-
-          count[genres[i]] = (count[genres[i]] || 0) + plays[i];
-        }
-
-        const topGenres = Object.entries(count).sort((a, b) => b[1] - a[1]);
-
-        for (let entry of Object.entries(hash)) {
-          const [genre, arr] = entry;
-          hash[genre] = arr.sort((a, b) => b[0] - a[0]);
-        }
-
-        const ans = [];
-
-        for (let entry of topGenres) {
-          const genre = entry[0];
-
-          for (let i = 0; i < 2; i++) {
-            if (hash[genre][i]) {
-              ans.push(hash[genre][i][1]);
-            }
-          }
-        }
-
-        return ans;
-      }
     }
   }
 
