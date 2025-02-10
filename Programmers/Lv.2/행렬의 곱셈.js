55e87c8fb74c9efc08cc62521d6a0d20b1adb801
soyeon <jayy_19@ewhain.net>
2025-02-10 17:05:18 +0900

    [Fix] Programmers Lv2: 행렬의 곱셈

--- Programmers/Lv.2/행렬의 곱셈.js
+++ Programmers/Lv.2/행렬의 곱셈.js
@@ -1,12 +1,14 @@
 function solution(arr1, arr2) {
   const N = arr1.length;
-  const M = arr1[0].length;
+  const K = arr1[0].length;
+  const M = arr2[0].length;
+
   const ans = Array.from({ length: N }, () => Array(M).fill(0));
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
       let sum = 0;
-      for (let k = 0; k < M; k++) {
+      for (let k = 0; k < K; k++) {
         sum += arr1[i][k] * arr2[k][j];
       }
       ans[i][j] = sum;
