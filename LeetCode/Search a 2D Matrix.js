0c45a8e0c0a9cb01522b66ca909fb1425ec5d07d
Xoeon <jayy_19@ewhain.net>
2025-04-28 17:22:23 +0900

    [Refactor] LeetCode: 74. Search a 2D Matrix

--- LeetCode/Search a 2D Matrix.js
+++ LeetCode/Search a 2D Matrix.js
@@ -11,23 +11,20 @@ var searchMatrix = function (matrix, target) {
 
   for (let i = 0; i < m; i++) {
     if (matrix[i][n - 1] < target) continue;
-    else {
-      let start = 0;
-      let end = n - 1;
 
-      while (start <= end) {
-        const mid = Math.floor((start + end) / 2);
+    let start = 0;
+    let end = n - 1;
 
-        if (matrix[i][mid] === target) {
-          return true;
-        } else if (matrix[i][mid] < target) {
-          start = mid + 1;
-        } else if (matrix[i][mid] > target) {
-          end = mid - 1;
-        }
+    while (start <= end) {
+      const mid = Math.floor((start + end) / 2);
+
+      if (matrix[i][mid] === target) {
+        return true;
+      } else if (matrix[i][mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
       }
-
-      return false;
     }
   }
 
