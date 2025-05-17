fdca7d8953fe5f202818131afad07cab7731dc9d
Xoeon <jayy_19@ewhain.net>
2025-05-17 17:18:35 +0900

    [Refactor]Programmers Lv.3: 다단계 칫솔 판매

--- Programmers/Lv.3/다단계 칫솔 판매.js
+++ Programmers/Lv.3/다단계 칫솔 판매.js
@@ -1,24 +1,29 @@
 function solution(enroll, referral, seller, amount) {
   const PRICE = 100;
+  const PERCENT = 0.1;
+  const ROOT = "center";
 
-  const parent = { center: null };
-  for (let i = 0; i < referral.length; i++) {
-    parent[enroll[i]] = referral[i] === "-" ? "center" : referral[i];
+  const parent = { [ROOT]: null };
+  const earnings = {};
+
+  // parent, earnings 초기화
+  for (let i = 0; i < enroll.length; i++) {
+    parent[enroll[i]] = referral[i] === "-" ? ROOT : referral[i];
+    earnings[enroll[i]] = 0;
   }
 
-  const sum = {};
-  const splitMoney = (vendor, money) => {
-    if (vendor === "center" || money <= 0) return;
-
-    const p = parent[vendor];
-    const pm = Math.floor(money * 0.1);
-    sum[vendor] = (sum[vendor] || 0) + (money - pm);
-    splitMoney(p, pm);
-  };
-
+  // 판매 수익 분배
   for (let i = 0; i < seller.length; i++) {
-    splitMoney(seller[i], amount[i] * PRICE);
+    let vendor = seller[i];
+    let profit = amount[i] * PRICE;
+
+    while (vendor !== ROOT && profit > 0) {
+      const commission = Math.floor(profit * PERCENT);
+      earnings[vendor] += profit - commission;
+      vendor = parent[vendor];
+      profit = commission;
+    }
   }
 
-  return enroll.map((v) => (sum[v] ? sum[v] : 0));
+  return enroll.map((name) => earnings[name]);
 }
